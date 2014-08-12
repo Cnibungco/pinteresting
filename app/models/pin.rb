f673412aa45739c624887338f1e2d42b57e328b2
Camille Nibungco <ccnibungco@csupomona.edu>
2014-08-11 23:57:01 -0700

    add name to users

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -2,4 +2,6 @@ class Pin < ActiveRecord::Base
 	belongs_to :user
 	
 	has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
+  validates :description, presence: true
+  validates :image, presence: true
 end
